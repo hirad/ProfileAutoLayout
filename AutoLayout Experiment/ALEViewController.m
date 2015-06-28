af3fe218a7981acc14df6430a15bbcd573a5fd18
Hirad Motamed <hirad@pendarlabs.com>
2015-06-27 22:39:07 -0700

    Adjusted behaviour for persistent negative translations. Added transparency effect to labels.

--- AutoLayout Experiment/ALEViewController.m
+++ AutoLayout Experiment/ALEViewController.m
@@ -11,9 +11,12 @@
 
 CGFloat const kDefaultHeaderHeight = 120.0;
 CGFloat const kDefaultProfilePictureWidth = 70.0;
+CGFloat const kDefaultProfilePictureLead = 30.0;
 CGFloat const kSmallProfilePictureWidth = 40.0;
 CGFloat const kShiftThreshold = -50.0; // translation after which to move the profile pic behind.
 CGFloat const kDefaultProfilePictureOffset = -30.0;
+CGFloat const kTranslationForFullyTransparentHeaderNameLabel = -110;
+CGFloat const kTranslationForFullyOpaqueHeaderNameLabel = -150;
 
 CGFloat translationMultiplierForCurrentValue(CGFloat value) {
     CGFloat adjustedValue = value;
@@ -28,6 +31,18 @@ CGFloat translationMultiplierForCurrentValue(CGFloat value) {
 
 CGFloat currentValueForMultiplier(CGFloat multiplier, CGFloat start, CGFloat end) {
     return multiplier * (end - start) + start;
+}
+
+CGFloat boundedNumber(CGFloat number, CGFloat min, CGFloat max) {
+    if (number < min) {
+        return min;
+    }
+    else if (number > max) {
+        return max;
+    }
+    else {
+        return number;
+    }
 }
 
 
@@ -67,6 +82,7 @@ CGFloat currentValueForMultiplier(CGFloat multiplier, CGFloat start, CGFloat end
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *profilePictureOffsetConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *contentAndHeaderAttachmentConstraint;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *headerLabelTopConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *profilePictureLeadingConstraint;
 
 @property (assign, nonatomic) CGFloat currentOffset;
 @end
@@ -85,10 +101,6 @@ CGFloat currentValueForMultiplier(CGFloat multiplier, CGFloat start, CGFloat end
 //    self.headerNameLabel.hidden = self.headerHandleLabel.hidden = YES;
 }
 
--(void)viewDidAppear:(BOOL)animated{
-    [self.view sendSubviewToBack:self.headerView];
-}
-
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
@@ -96,30 +108,37 @@ CGFloat currentValueForMultiplier(CGFloat multiplier, CGFloat start, CGFloat end
 }
 
 - (IBAction)moveContentView:(UIPanGestureRecognizer *)sender {
-    CGFloat yOffset = [sender translationInView:self.view].y;
+    CGFloat yTranslation = [sender translationInView:self.view].y;
+    CGFloat yOffset = self.currentOffset + yTranslation;
     
     switch (sender.state) {
         case UIGestureRecognizerStateBegan:
-            [self animateWithTranslation:yOffset animated:NO];
+            [self panViewWithTranslation:yOffset animated:NO];
             break;
         case UIGestureRecognizerStateChanged:
-            [self animateWithTranslation:yOffset animated:NO];
+            [self panViewWithTranslation:yOffset animated:NO];
             break;
         case UIGestureRecognizerStateEnded:
-            yOffset < 0 ? : [self animateWithTranslation:0.0 animated:YES];
-            self.currentOffset += yOffset;
+            if (yOffset < 0) {
+                self.currentOffset = yOffset;
+            }
+            else {
+                [self panViewWithTranslation:0.0 animated:YES];
+                self.currentOffset = 0;
+            }
         default:
             break;
     }
 }
 
--(void)animateWithTranslation:(CGFloat)translation animated:(BOOL)animated {
+-(void)panViewWithTranslation:(CGFloat)translation animated:(BOOL)animated {
     CGFloat distance = self.headerViewHeightConstraint.constant - kDefaultHeaderHeight;
     CGFloat newHeaderHeight = kDefaultHeaderHeight + translation;
     
     CGFloat multiplier = translationMultiplierForCurrentValue(translation);
     CGFloat newProfilePictureOffset = currentValueForMultiplier(multiplier, kDefaultProfilePictureOffset, 0.0);
     CGFloat newProfilePictureWidth = currentValueForMultiplier(multiplier, kDefaultProfilePictureWidth, kSmallProfilePictureWidth);
+    CGFloat profilePictureLeadingAdjustment = (int)((kDefaultProfilePictureWidth - newProfilePictureWidth)/2 + 0.5);
     
     CGFloat contentViewHeightOffset;
     if (translation < kShiftThreshold) {
@@ -133,30 +152,38 @@ CGFloat currentValueForMultiplier(CGFloat multiplier, CGFloat start, CGFloat end
     }
     
     // show the header labels if the current name label has crossed the header
-//    CGFloat nameLabelY = [self.view convertPoint:self.nameLabel.frame.origin fromView:self.contentView].y;
-//    self.headerHandleLabel.hidden = self.headerNameLabel.hidden = (nameLabelY < newHeaderHeight);
+    CGFloat nameLabelY = [self.view convertPoint:self.nameLabel.frame.origin fromView:self.contentView].y;
+    if (nameLabelY < newHeaderHeight) {
+        CGFloat delta = newHeaderHeight - nameLabelY;
+        CGFloat nameLabelAlpha = boundedNumber((translation - kTranslationForFullyOpaqueHeaderNameLabel)/(kTranslationForFullyTransparentHeaderNameLabel - kTranslationForFullyOpaqueHeaderNameLabel), 0, 1);
+        CGFloat headerNameLabelAlpha = 1 - nameLabelAlpha;
+        self.headerNameLabel.alpha = self.headerHandleLabel.alpha = headerNameLabelAlpha;
+        self.nameLabel.alpha = self.handleLabel.alpha = nameLabelAlpha;
+        NSLog(@"Label Delta: %f at translation: %f\tAlpha: %f", delta, translation, nameLabelAlpha);
+    }
     CGFloat headerLabelOffset = MAX(kDefaultHeaderHeight + contentViewHeightOffset, 20.0);
     
-    NSLog(@"Xlation: %f\tOffset: %f\tHeader: %f\tHeader Offset: %f",
-          translation, newProfilePictureOffset, newHeaderHeight, contentViewHeightOffset);
+//    NSLog(@"Xlation: %f\tOffset: %f\tHeader: %f\tHeader Offset: %f",
+//          translation, newProfilePictureOffset, newHeaderHeight, contentViewHeightOffset);
     
     self.headerLabelTopConstraint.constant = headerLabelOffset;
     self.headerViewHeightConstraint.constant = newHeaderHeight;
     self.contentAndHeaderAttachmentConstraint.constant = contentViewHeightOffset;
     self.profilePictureOffsetConstraint.constant = newProfilePictureOffset;
     self.profilePictureWidthConstraint.constant = newProfilePictureWidth;
+    self.profilePictureLeadingConstraint.constant = kDefaultProfilePictureLead + profilePictureLeadingAdjustment;
     [self.view setNeedsUpdateConstraints];
     void (^animationBlock)() = ^{
         [self.view layoutIfNeeded];
     };
     
     if (animated) {
-        CGFloat speed = 100.0; // px/sec
+        CGFloat speed = 200.0; // px/sec
         NSTimeInterval duration = ceil(fabs(distance/speed));
         [UIView animateWithDuration:duration
                               delay:0.0
-             usingSpringWithDamping:0.95
-              initialSpringVelocity:0.85
+             usingSpringWithDamping:0.8
+              initialSpringVelocity:0.9
                             options:UIViewAnimationOptionCurveEaseOut
                          animations:animationBlock
                          completion:nil];
